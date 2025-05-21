9b64a48ab4f3410696a50bed0a3c14912c971f86
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-21 23:53:59 +0900

    WIP

--- src/app/(pages)/about/sections/PartsNCurriculum/PartsNCurriculum.tsx
+++ src/app/(pages)/about/sections/PartsNCurriculum/PartsNCurriculum.tsx
@@ -1,7 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import Section from "@/components/ui/Section";
+import PartSelector, { Parts } from "./components/PartSelector";
+
 export default function PartsNCurriculum() {
+    const [selectedPart, setSelectedPart] = useState<Parts>("기획");
+
     return (
-        <div>
-            <p>PartsNCurriculum</p>
-        </div>
+        <Section
+            displayName="Parts & Curriculum"
+            displayTitle="5개의 파트로 운영되고 있어요"
+            theme="LIGHT"
+            className="py-[200px]"
+        >
+            <div>
+                <PartSelector selectedPart={selectedPart} onChange={setSelectedPart} />
+
+                <div className="text-center text-xl font-semibold">
+                    현재 선택된 파트: {selectedPart}
+                </div>
+            </div>
+        </Section>
     );
 }
