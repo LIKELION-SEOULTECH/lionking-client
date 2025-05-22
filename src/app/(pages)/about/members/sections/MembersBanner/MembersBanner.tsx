ac1d120db221e0edb3374577ca4456e4a42b23ee
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-22 11:18:32 +0900

    fix: typo(#28)

--- src/app/(pages)/about/members/sections/MembersBanner/MembersBanner.tsx
+++ src/app/(pages)/about/members/sections/MembersBanner/MembersBanner.tsx
@@ -3,7 +3,7 @@ import MembersBannerSVG from "@/assets/about/banner_members.svg";
 export default function MembersBanner() {
     return (
         <div className="pt-[62px]">
-            <MembersBannerSVG />;
+            <MembersBannerSVG />
         </div>
     );
 }
