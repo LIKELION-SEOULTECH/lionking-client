efadc16042c19fbc5f8816cbc3f7b8094a71ba77
Dohyeop Lim <dhlim0817@gmail.com>
2025-05-30 02:22:16 +0900

    feat: add dashboard action buttons(#56)

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import type { PostPreviewMetadata } from "@/types";
 import DashboardMetricCardRow from "./components/DashboardMetricCardRow";
 import DashboardProfileCard from "./components/DashboardProfileCard";
 import DashboardPublishedBlogs from "./components/DashboardPublishedBlogs";
+import DashboardActionButton from "./components/DashboardActionButton";
 
 const mockMetrics = [
     {
@@ -44,17 +45,29 @@ export default function DashboardPage() {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[auto_1fr] gap-10 w-full">
             <div className="h-fit lg:sticky">
-                <DashboardProfileCard
-                    member={{
-                        id: 1,
-                        name: "김먀옹",
-                        major: "인공지능학과",
-                        position: "프론트엔드",
-                        role: "아기사자",
-                        userTags: ["프론트엔드", "아기사자"],
-                    }}
-                />
-                버튼..
+                <div className="w-full flex flex-col items-start justify-start gap-7">
+                    <DashboardProfileCard
+                        member={{
+                            id: 1,
+                            name: "김먀옹",
+                            major: "인공지능학과",
+                            position: "프론트엔드",
+                            role: "아기사자",
+                            userTags: ["프론트엔드", "아기사자"],
+                        }}
+                    />
+
+                    <div className="w-full flex flex-col items-start justify-start gap-2.5">
+                        <DashboardActionButton label="새 글 작성하기" />
+                        {/* <DashboardActionButton label="회원 관리" /> */}
+                    </div>
+
+                    <div className="w-full flex flex-col items-center justify-center">
+                        <button className="sub3_sb text-gray-4 underline hover:text-gray-5 transition-colors duration-200 cursor-pointer">
+                            아이디/비밀번호 변경
+                        </button>
+                    </div>
+                </div>
             </div>
 
             <div className="w-full flex flex-col items-start justify-start gap-10 mt-8 md:mt-0">
