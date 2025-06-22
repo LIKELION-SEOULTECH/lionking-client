7a178cfd34eabbf68cb3caeaed5b190ceb460142
Dohyeop Lim <dhlim0817@gmail.com>
2025-06-22 19:08:36 +0900

    feat: add lottie animation to about banner(#68)

--- src/app/(pages)/about/sections/AboutBanner/AboutBanner.tsx
+++ src/app/(pages)/about/sections/AboutBanner/AboutBanner.tsx
@@ -1,20 +1,39 @@
 "use client";
 
-import Lottie from "lottie-react";
+import { useRef, useState } from "react";
+import Lottie, { LottieRefCurrentProps } from "lottie-react";
 
 import BannerBackground from "@/assets/banner/about/banner_bg.svg";
 import icons from "@/assets/banner/about/team/icons.json";
 
 export default function AboutBanner() {
+    const lottieRef = useRef<LottieRefCurrentProps>(null);
+    const [direction, setDirection] = useState<1 | -1>(1);
+
+    const handleComplete = () => {
+        if (!lottieRef.current) return;
+
+        const nextDir = (direction === 1 ? -1 : 1) as 1 | -1;
+        setDirection(nextDir);
+        lottieRef.current.setDirection(nextDir);
+
+        const totalFrames = lottieRef.current.getDuration(true);
+        const jumpToFrame = nextDir === 1 ? 0 : totalFrames;
+
+        lottieRef.current.goToAndPlay(jumpToFrame ?? 0, true);
+    };
+
     return (
-        <div className="relative w-full h-[528px] pt-[62px] overflow-hidden">
-            <BannerBackground className="absolute inset-0 w-full h-full object-cover z-0" />
+        <div className="relative w-full max-h-[500px] pt-[60px] overflow-hidden">
+            <BannerBackground className="relative w-full h-full" />
 
             <Lottie
+                lottieRef={lottieRef}
                 animationData={icons}
-                loop={true}
                 autoplay={true}
-                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-10"
+                loop={false}
+                onComplete={handleComplete}
+                className="absolute top-0 left-0 w-full h-full pointer-events-none pt-[60px]"
             />
         </div>
     );
