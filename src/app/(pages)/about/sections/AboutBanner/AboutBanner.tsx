a743bb5f3f58065b6f74ada14309a0916e8b3d8f
Dohyeop Lim <dhlim0817@gmail.com>
2025-06-22 14:31:34 +0900

    wip

--- src/app/(pages)/about/sections/AboutBanner/AboutBanner.tsx
+++ src/app/(pages)/about/sections/AboutBanner/AboutBanner.tsx
@@ -1,9 +1,21 @@
-import AboutBannerSVG from "@/assets/about/banner_intro.svg";
+"use client";
+
+import Lottie from "lottie-react";
+
+import BannerBackground from "@/assets/banner/about/banner_bg.svg";
+import icons from "@/assets/banner/about/team/icons.json";
 
 export default function AboutBanner() {
     return (
-        <div className="pt-[62px]">
-            <AboutBannerSVG />
+        <div className="relative w-full h-[528px] pt-[62px] overflow-hidden">
+            <BannerBackground className="absolute inset-0 w-full h-full object-cover z-0" />
+
+            <Lottie
+                animationData={icons}
+                loop={true}
+                autoplay={true}
+                className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-full z-10"
+            />
         </div>
     );
 }
