163b8c8e49087a480d483f7b39fb09f1515aefef
Kim Minseo <minqseo0630@naver.com>
2025-07-01 00:49:37 +0900

    ui 수정완료

--- src/app/(pages)/notice/[noticeId]/components/DetailBody.tsx
+++ src/app/(pages)/notice/[noticeId]/components/DetailBody.tsx
@@ -24,7 +24,7 @@ export default function DetailBody({
     attachment,
     resourceName = "게시물",
 }: DetailBodyProps) {
-    const router = useRouter();
+    // const router = useRouter();
     const { noticeId } = useParams() as { noticeId: string };
 
     const handleDelete = async () => {
