f5d3479f22965ec0de268f1f232d66ccac0cff28
Dohyeop Lim <dhlim0817@gmail.com>
2025-07-02 16:04:21 +0900

    fix: gallery page UI(#88)

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -1,13 +1,26 @@
 import GalleryBanner from "./sections/GalleryBanner";
 import NewsList from "./sections/NewsList";
 import Activities from "./sections/Activities";
+import { get_activity } from "@/lib/api/endpoints/activity";
 
-export default function GalleryPage() {
+export default async function GalleryPage() {
+    const activities = await (async () => {
+        try {
+            const result = await get_activity();
+            return result ?? [];
+        } catch {
+            return [];
+        }
+    })();
+
     return (
         <>
             <GalleryBanner />
-            <NewsList />
-            <Activities />
+
+            <div className="w-full flex flex-col items-start justify-start py-30 px-6 lg:px-4 xl:px-0 gap-30">
+                <NewsList items={activities} />
+                <Activities items={activities} />
+            </div>
         </>
     );
 }
